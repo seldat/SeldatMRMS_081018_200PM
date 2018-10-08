0008563890a1552894efe08ba9e87299bad2fb42
luattran <seldatSWT@gmail.com>
2018-10-08 16:50:20 +0700

    luat ne

--- SeldatMRMS/Management/FormManager/LiveViewForm.cs
+++ SeldatMRMS/Management/FormManager/LiveViewForm.cs
@@ -104,6 +104,7 @@ namespace SeldatMRMS.Management.FormManager
         private void LiveViewForm_Load(object sender, EventArgs e)
         {
             timer1.Start();
+            // ok
             Task.Run(() =>
             {
                 cameraStrt.capture = new VideoCapture(cameraStrt.url);
